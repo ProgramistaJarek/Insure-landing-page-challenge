ca3a5a58550f1986b6726a564d60befdefaca00f
Jarosław Lepich <dojarka0@gmail.com>
2020-06-15 21:14:34 +0200

    js menu show and hide on click and hover on touch screen

--- javascript.js
+++ javascript.js
@@ -1,5 +1,48 @@
-var menuBtn = document.querySelector("img .menubutton");
+window.onload = function () {
+  var menuBtn = document.querySelector(".menu-img");
+  var menuList = document.querySelector(".active");
+  var menu = document.querySelectorAll("ul li");
 
-menuBtn.addEventListener("click", function(){
-    console.log("działa");
-});
+  menuBtn.addEventListener("click", function () {
+    if (menuList.style.display == "none") {
+      menuList.style.display = "block";
+      menuBtn.style.backgroundImage = "url(./images/icon-close.svg)";
+    } else {
+      menuList.style.display = "none";
+      menuBtn.style.backgroundImage = "url(./images/icon-hamburger.svg)";
+    }
+  });
+  var liHover =
+    "ul li:hover {border: 1px solid white;margin: 0 20px;padding: calc(1rem - 1px) 0;}";
+
+  for (var i = 0; i < menu.length; i++) {
+    menu[i].addEventListener(
+      "touchstart",
+      function () {
+        this.classList.add("liHover");
+      },
+      true
+    );
+    menu[i].addEventListener(
+      "touchend",
+      function () {
+        this.classList.remove("liHover");
+      },
+      true
+    );
+    menu[i].addEventListener(
+      "mouseenter",
+      function () {
+        this.classList.add("liHover");
+      },
+      true
+    );
+    menu[i].addEventListener(
+      "mouseout",
+      function () {
+        this.classList.remove("liHover");
+      },
+      true
+    );
+  }
+};
